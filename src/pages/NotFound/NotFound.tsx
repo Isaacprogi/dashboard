4472259e3a6c0e34fd1edb81a2c82373024e89cc
Isaac <isaaconyes80@gmail.com>
2024-04-18 00:12:22 +0100

    update

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold text-gray-700 mb-2">404 - Not Found</h1>
         <p className="text-gray-500 mb-4">Oops! The page you're looking for doesn't exist.</p>
-        <Link to="/" className="border text-black hover:bg-gray-200 text-white font-bold py-2 px-4 rounded">
+        <Link to="/" className="border text-black hover:bg-gray-200  font-bold py-2 px-4 rounded">
           Go Home
         </Link>
       </div>
